abe51033909339196702db123a02795e1202687f
OROCHI <tungus_v@yahoo.co.jp>
2015-06-12 00:57:50 +0900

    作業用プログラム追加

---
+++ Programs/Scene_Source_maker_madoka/Scene_Source_maker_madoka/Names.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scene_Source_maker_madoka
+{
+    class Names
+    {
+        public Names()
+        {
+        }
+        // 名前リスト(ここのリストを検索して引っかからなかったキャラはそのまま使う)
+        // 第1次元：キャラクター
+        // 第2次元1：シナリオ上の名前
+        // 第2次元2：表示する名前（本名）
+        // 第2次元3：表示する名前（英語）
+        // 第2次元4：ノーマル顔のインデックス
+        public string[][] CharacterNameList = new string[][]
+        {
+            new string[] {""},      // ダミー
+            new string[] {"まどか","鹿目まどか","KANAME MADOKA", "CharacterFace.MADOKA_NORMAL"},     // まどか
+            new string[] {"さやか", "美樹さやか","MIKI SAYAKA","CharacterFace.SAYAKA_NORMAL"},       // さやか
+            new string[] {"マミ","巴マミ","TOMOE MAMI","CharacterFace.MAMI_NORMAL"},               // マミ
+            new string[] {"ほむら","暁美ほむら","AKEMI HOMURA","CharacterFace.HOMURA_NORMAL"},       // ほむら
+            new string[] {"杏子","佐倉杏子","SAKURA KYOKO","CharacterFace.KYOKO_NORMAL"},           // 杏子
+            new string[] {"ゆま","千歳ゆま","CHITOSE YUMA","CharacterFace.YUMA_NORMAL"},           // ゆま
+            new string[] {"織莉子","美国織莉子","MIKUNI ORIKO","CharacterFace.ORIKO_NORMAL"},       // 織莉子
+            new string[] {"キリカ","呉キリカ","KURE KIRIKA","CharacterFace.KIRIKA_NORMAL"},          // キリカ
+            new string[] {"スコノシュート","SCONOSCIUTO","SCONOSCIUTO","CharacterFace.SCONOSCIUTO_NORMAL"},// スコノシュート
+            new string[] {"アルティメットまどか","アルティメットまどか","ULTIMATE MADOKA","CharacterFace.ULTIMATE_MADOKA_NORMAL"},
+            new string[] {"さやかゴッドシブ","さやかゴッドシブ","SAYAKA GODSIBB","CharacterFace.SAYAKA_GODSIBB_NORMAL"},
+            new string[] {"恭介","上条恭介","KAMIJOU KYOUSUKE","CharacterFace.KYOSUKE_NORMAL"},
+            new string[] {"仁美","志筑仁美","SHIZUKI HITOMI","CharacterFace.HITOMI_NORMAL"},
+            new string[] {"詢子","鹿目詢子","KANAME JUNKO","CharacterFace.JUNKO_NORMAL"},
+            new string[] {"知久","鹿目知久","KANAME TOMOHISA","CharacterFace.TOMOHISA_NORMAL"},
+            new string[] {"タツヤ","鹿目タツヤ","KANAME TATSUYA","CharacterFace.TATSUYA_NORMAL"},
+            new string[] {"和子","早乙女和子","SAOTOME KAZUKO","CharacterFace.KAZUKO_NORMAL"},
+            new string[] {"中沢","中沢","NAKAZAWA","CharacterFace.NAKAZAWA_NORMAL"},
+            new string[] {"ミッチェル","ミッチェル・ノートルダム","MICHELLE NOSTREDAME","CharacterFace.MICHEL_NORMAL"},
+            new string[] {"キュゥべえ","キュゥべえ","KYUBEY","CharacterFace.KYUBEY_NORMAL"}
+        };        
+    }
+}
---
+++ Programs/Scene_Source_maker_madoka/Scene_Source_maker_madoka/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+
+namespace Scene_Source_maker_madoka
+{
+    class Program
+    {
+        // 最大表示行数
+        public const int max_Line = 4;       
+
+        // 1行当たりの文字数
+        public const int max_Words = 30;
+
+        // 元データが記載されているファイル
+        public const string origin_file = "original.txt";
+
+        // 出力結果を保存するファイル
+        public const string result_file = "result.txt";
+        
+        // 名前データを格納したクラス
+        static public Names names = null;   // 共有させるならstaticが必須
+
+        static void Main(string[] args)
+        {
+            names = new Names();
+
+            // ファイルを読み込む（こっちはshift-jisで構わない）
+            string[] lines1 = File.ReadAllLines(origin_file, System.Text.Encoding.GetEncoding("Shift_JIS"));
+
+            // 取得内容によって変える
+            for (int a = 0; a < lines1.Length; a++)
+            {
+                string OR_string = lines1[a];
+                // 長さを取得する
+                int word_le = OR_string.Length;
+                // 何もない行は読まない
+                if (OR_string != "")
+                {
+                    // case を書き込む
+                    File.AppendAllText(result_file, "case " + a + ":\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+
+                    // :までを切り出す 
+                    int iFind1 = OR_string.IndexOf('：');
+
+                    // :以前を名前とする
+                    string name = OR_string.Substring(0, iFind1);
+                    // ここで：前の名前が誰であったかによって処理分岐
+                    drawserif(a, name, OR_string, word_le, iFind1);
+                          
+                }
+                // breakを書き込む
+                File.AppendAllText(result_file, "break;\r\n", System.Text.Encoding.GetEncoding("Shift_JIS"));
+            }
+            Console.WriteLine("COMPLETE!!");
+            Console.ReadLine();
+        }
+
+        // 名前をもとにセリフを書く
+        // 共有させるならstaticが必須
+        // 第1引数：ループ番号
+        // 第2引数：名前
+        // 第3引数：読み取った文字列
+        // 第4引数：文字列の長さ
+        // 第5引数：コロン前までの長さ
+        static void drawserif(int a, string name, string OR_string, int word_le, int iFind1)
+        {
+            //for (int i = 0; i < names.CharacterNameList.GetLength(0); i++)
+            {
+                // 名前が登録されている誰であるかを取得する（いなければ0を返す）
+                int i = CheckName(name);
+                // ＊（全角アスタ）の場合xstory強制追加かつKeybreak=trueの空caseを入れる
+                if (name == "＊")
+                {
+                    File.AppendAllText(result_file, "keybreak = true;\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                    File.AppendAllText(result_file, "FullClear();\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                    File.AppendAllText(result_file, "IncrementXstory();\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                    return;
+                }
+                // ＊＊（全角アスタ2つ）の場合xstoryを強制追加しない空caseを入れる
+                else if (name == "＊＊")
+                {
+                    File.AppendAllText(result_file, "keybreak = true;\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                    File.AppendAllText(result_file, "FullClear();\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                    return;
+                }
+                // ＊＊＊（全角アスタ3つ）の場合名前を出さないcaseを入れる
+                else if (name == "＊＊＊")
+                {
+                    File.AppendAllText(result_file, "keybreak = false;\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                    File.AppendAllText(result_file, "FullClear();\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                    return;
+                }
+                // それ以外の場合表記された名前での台詞を出すcaseを入れる（その場合英文はなし）
+                else if (name != "")
+                {
+                    File.AppendAllText(result_file, "keybreak = false;\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                    // Namesに定義されている名前だった場合、本名と英文表記を追加して台詞を出すcaseを入れる                           
+                    // 定義あり
+                    if (i != 0)
+                    {
+                        // 顔
+                        File.AppendAllText(result_file, "m_facetype = " +  names.CharacterNameList[i][3] + ";\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                        // 名前日本語
+                        File.AppendAllText(result_file, "m_drawname_jp = " + "\"" + names.CharacterNameList[i][1] + "\"" + ";\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                        // 名前英語
+                        File.AppendAllText(result_file, "m_drawname_en = " + "\"" + names.CharacterNameList[i][2] + "\"" + ";\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                    }
+                    // 定義なし
+                    else
+                    {
+                        // 顔
+                        File.AppendAllText(result_file, "m_facetype = " + "0" + ";\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                        // 名前日本語
+                        File.AppendAllText(result_file, "m_drawname_jp = \"" + name + "\"" + ";\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                        // 名前英語
+                        File.AppendAllText(result_file, "m_drawname_en = \"" + "\"" + ";\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                    }
+                    // セリフ
+                    string[] word = new string[max_Line];
+
+
+                    if (word_le - iFind1 < 31)
+                    {
+                        word[0] = OR_string.Substring(iFind1 + 1);
+                    }
+                    else
+                    {
+                        word[0] = OR_string.Substring(iFind1 + 1, 30);
+                    }
+                    File.AppendAllText(result_file, "m_serif[0] = \"" + word[0] + "\";\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+
+                    // 2行以上にわたる場合
+                    for (int j = 1; j < max_Line; j++)
+                    {
+                        if (word_le - iFind1 > 30 * j)
+                        {
+                            if (word_le - iFind1 < 31 + 31 * j)
+                            {
+                                word[j] = OR_string.Substring(iFind1 + 31 + 30 * (j - 1));
+                            }
+                            else
+                            {
+                                word[j] = OR_string.Substring(iFind1 + 31 + 30 * (j - 1), 30);
+                            }
+                            File.AppendAllText(result_file, "m_serif[" + j + "] = \"" + word[j] + "\";\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                        }
+                        else
+                        {
+                            File.AppendAllText(result_file, "m_serif[" + j + "] = \""  + "\";\r\n", System.Text.Encoding.GetEncoding("UTF-8"));
+                        }
+                    }
+                    return;
+                }
+            }              
+        }
+
+
+        // 該当する名前をチェックする
+        // 第1引数：元データにおけるキャラクターの名前
+        static int CheckName(string inputname)
+        {
+            int name_Result = 0;
+
+            // 全員分ループを回し、見つかったインデックスを返す
+            for (int i = 0; i < names.CharacterNameList.GetLength(0); i++)
+            {
+                if (names.CharacterNameList[i][0] == inputname)
+                {
+                    return i;
+                }
+            }
+
+            return name_Result;
+        }
+        
+    }
+}
