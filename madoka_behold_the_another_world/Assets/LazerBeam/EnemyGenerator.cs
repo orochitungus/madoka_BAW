ab39d567831e3dabe904fbe28b12bfb1214525b9
OROCHI <tungus_v@yahoo.co.jp>
2015-06-21 01:16:59 +0900

    LazerBeamを追加

---
+++ madoka_behold_the_another_world/Assets/LazerBeam/EnemyGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGenerator : MonoBehaviour {
+    public GameObject enemyInst;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 40; ++i)
+        {
+            GameObject o = GameObject.Instantiate(enemyInst) as GameObject;
+            o.transform.position = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), 0f);
+        }
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+	}
+}
