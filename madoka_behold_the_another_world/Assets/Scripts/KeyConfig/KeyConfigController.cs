06e6b8733c3828b5acbf65ff905503dd9de5928a
orochitungus <tungus_v@yahoo.co.jp>
2016-03-09 20:00:18 +0900

    キーボードの取り方確定

--- madoka_behold_the_another_world/Assets/Scripts/KeyConfig/KeyConfigController.cs
+++ madoka_behold_the_another_world/Assets/Scripts/KeyConfig/KeyConfigController.cs
@@ -1,25 +1,133 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
 using System.Collections;
+using UniRx;
+using UniRx.Triggers;
 
-public class KeyConfigController : MonoBehaviour {
+public class KeyConfigController : MonoBehaviour
+{
+    /// <summary>
+    /// 射撃・決定ボタン
+    /// </summary>
+    public Button Shot;
 
-	// Use this for initialization
-	void Start () 
+    /// <summary>
+    /// 格闘ボタン
+    /// </summary>
+    public Button Wrestle;
+
+    /// <summary>
+    /// ジャンプ・キャンセルボタン
+    /// </summary>
+    public Button Jump;
+
+    /// <summary>
+    /// サーチボタン
+    /// </summary>
+    public Button Search;
+
+    /// <summary>
+    /// コマンドボタン
+    /// </summary>
+    public Button Command;
+
+    /// <summary>
+    /// メニューボタン
+    /// </summary>
+    public Button Menu;
+
+    /// <summary>
+    /// サブ射撃ボタン
+    /// </summary>
+    public Button SubShot;
+
+    /// <summary>
+    /// 特殊射撃ボタン
+    /// </summary>
+    public Button ExShot;
+
+    /// <summary>
+    /// 特殊格闘ボタン
+    /// </summary>
+    public Button ExWrestle;
+
+    /// <summary>
+    /// 視点回転上ボタン
+    /// </summary>
+    public Button ElevationAngleUpper;
+
+    /// <summary>
+    /// 視点回転下ボタン
+    /// </summary>
+    public Button ElevationAngleDown;
+
+    /// <summary>
+    /// 視点回転左ボタン
+    /// </summary>
+    public Button AzimuthLeft;
+
+    /// <summary>
+    /// 視点回転左ボタン
+    /// </summary>
+    public Button AzimuthRight;
+
+
+    /// <summary>
+    /// どれが選択されているか
+    /// </summary>
+    public enum NOWSELECT
+    {
+        SHOT,
+        WRESTLE,
+        JUMP,
+        SEARCH,
+        COMMAND,
+        MENU,
+        SUBSHOT,
+        EXSHOT,
+        EXWRESTLE,
+        ELEVATIONUPPER,
+        ELEVETIONDOWN,
+        AZIMUTHLEFT,
+        AZIMUTHRIGHT
+    }
+
+    // 各カーソル
+    public Image ShotKeyboard;
+    public Image ShotController;
+
+    public Image WrestleKeyboard;
+    public Image WrestleController;
+
+    public Image JumpKeyboad;
+    public Image JumpController;
+
+    public NOWSELECT Nowselect;
+
+    // Use this for initialization
+    void Start () 
 	{
-		string[] js = Input.GetJoystickNames();
-		for(int i=0; i<js.Length; i++)
-		{
-			Debug.Log(js[i]);
-		}
+        // 選択対象を初期化
+        Nowselect = NOWSELECT.SHOT;
+        
+        // 各オブジェクトを選択中、カーソルカラーを赤に変化
+        		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.inputString != "")
-		{
-			Debug.Log(Input.inputString);
-		}
-		
-	}
+        if (Input.anyKeyDown)
+        {
+            Array k = Enum.GetValues(typeof(KeyCode));
+            for (int i = 0; i < k.Length; i++)
+            {
+                if (Input.GetKeyDown((KeyCode)k.GetValue(i)))
+                {
+                    Debug.Log(k.GetValue(i).ToString());
+                }
+            }
+        }
+    }
 }
