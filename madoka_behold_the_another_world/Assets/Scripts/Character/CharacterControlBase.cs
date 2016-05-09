0576c5658b9b7cb6b54dea756ad1d63bf9926aac
OROCHI <tungus_v@yahoo.co.jp>
2016-05-10 08:09:21 +0900

    ccbを更新

--- madoka_behold_the_another_world/Assets/Scripts/Character/CharacterControlBase.cs
+++ madoka_behold_the_another_world/Assets/Scripts/Character/CharacterControlBase.cs
@@ -35,10 +35,54 @@ public class CharacterControlBase : MonoBehaviour
     /// <summary>
     /// このオブジェクトのRigidbody
     /// </summary>
-    protected Rigidbody RigidbodyObject;
+    protected Rigidbody RigidBody;
 
-    // Use this for initialization
-    void Start ()
+	// 哨戒モードにおける起点(僚機の時はPC）
+	public GameObject StartingPoint;
+	public GameObject EndingPoint;
+
+	// 歩き撃ちフラグ
+	protected bool RunShotDone;
+
+	/// <summary>
+	/// 覚醒技演出中フラグ
+	/// </summary>
+	public bool ArousalAttackProduction;
+
+	// プレイヤーであるか否か（これがfalseであると描画系の関数全カット）
+	// それぞれの意味は以下の通り
+	public enum CHARACTERCODE
+	{
+		PLAYER,         // プレイヤー操作
+		PLAYER_ALLY,    // プレイヤー僚機
+		ENEMY           // 敵
+	};
+	public CHARACTERCODE IsPlayer;
+
+	// 弾の種類(CharacterSpecの配置順に合わせること）
+	public enum ShotType
+	{
+		NORMAL_SHOT,    // 通常射撃
+		CHARGE_SHOT,    // チャージ射撃
+		SUB_SHOT,       // サブ射撃
+		EX_SHOT,        // 特殊射撃        
+	};
+
+	// 射撃時の射出モード
+	public enum ShotMode
+	{
+		NORMAL,     // 通常状態
+		RELORD,     // 構え
+		SHOT,       // 射出
+		SHOTDONE,   // 射出完了
+		AIRDASHSHOT // 射出（空中ダッシュ射撃）
+	};
+	public ShotMode Shotmode;
+
+
+
+	// Use this for initialization
+	void Start ()
     {
 	
 	}
