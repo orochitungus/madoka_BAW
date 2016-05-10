d7d874d83f8d6bb5b49286fda1bf5a7653700b62
orochitungus <tungus_v@yahoo.co.jp>
2016-05-10 18:36:33 +0900

    ccbを更新

--- madoka_behold_the_another_world/Assets/Scripts/Character/CharacterControlBase.cs
+++ madoka_behold_the_another_world/Assets/Scripts/Character/CharacterControlBase.cs
@@ -37,21 +37,27 @@ public class CharacterControlBase : MonoBehaviour
     /// </summary>
     protected Rigidbody RigidBody;
 
-	// 哨戒モードにおける起点(僚機の時はPC）
-	public GameObject StartingPoint;
+    /// <summary>
+    /// 哨戒モードにおける起点(僚機の時はPC）
+    /// </summary>
+    public GameObject StartingPoint;
 	public GameObject EndingPoint;
 
-	// 歩き撃ちフラグ
-	protected bool RunShotDone;
+    /// <summary>
+    /// 歩き撃ちフラグ
+    /// </summary>
+    protected bool RunShotDone;
 
 	/// <summary>
 	/// 覚醒技演出中フラグ
 	/// </summary>
 	public bool ArousalAttackProduction;
 
-	// プレイヤーであるか否か（これがfalseであると描画系の関数全カット）
-	// それぞれの意味は以下の通り
-	public enum CHARACTERCODE
+    /// <summary>
+    /// プレイヤーであるか否か（これがfalseであると描画系の関数全カット）
+    /// それぞれの意味は以下の通り
+    /// </summary>
+    public enum CHARACTERCODE
 	{
 		PLAYER,         // プレイヤー操作
 		PLAYER_ALLY,    // プレイヤー僚機
@@ -59,8 +65,10 @@ public class CharacterControlBase : MonoBehaviour
 	};
 	public CHARACTERCODE IsPlayer;
 
-	// 弾の種類(CharacterSpecの配置順に合わせること）
-	public enum ShotType
+    /// <summary>
+    /// 弾の種類(CharacterSpecの配置順に合わせること）
+    /// </summary>
+    public enum ShotType
 	{
 		NORMAL_SHOT,    // 通常射撃
 		CHARGE_SHOT,    // チャージ射撃
@@ -68,8 +76,10 @@ public class CharacterControlBase : MonoBehaviour
 		EX_SHOT,        // 特殊射撃        
 	};
 
-	// 射撃時の射出モード
-	public enum ShotMode
+    /// <summary>
+    /// 射撃時の射出モード
+    /// </summary>
+    public enum ShotMode
 	{
 		NORMAL,     // 通常状態
 		RELORD,     // 構え
@@ -79,10 +89,51 @@ public class CharacterControlBase : MonoBehaviour
 	};
 	public ShotMode Shotmode;
 
+    /// <summary>
+    /// 誰であるか
+    /// </summary>
+    public Character_Spec.CHARACTER_NAME CharacterName;
+
+    /// <summary>
+    /// 頭部オブジェクト
+    /// </summary>
+    public GameObject HeadObject;
+
+    /// <summary>
+    /// 胸部オブジェクト
+    /// </summary>
+    public GameObject BrestObject;
+
+    /// <summary>
+    /// 覚醒時エフェクト
+    /// </summary>
+    public GameObject ArousalEffect;
+
+    /// <summary>
+    /// ポーズコントローラー
+    /// </summary>
+    private GameObject Pausecontroller;
+
+    // CPU
+    /// <summary>
+    /// ルーチンを拾う
+    /// </summary>
+    AIControl_Base.CPUMODE CPUMode;
+
+    /// <summary>
+    /// テンキー入力を拾う
+    /// </summary>
+    AIControl_Base.TENKEY_OUTPUT CPUTenkey;
+
+    /// <summary>
+    /// キー入力を拾う
+    /// </summary>
+    AIControl_Base.KEY_OUTPUT CPUKey;
 
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start ()
     {
 	
 	}
