0ccbd280c26c487cfa70b50a5a850dca9c6318fb
orochitungus <tungus_v@yahoo.co.jp>
2016-05-23 19:37:09 +0900

    インターフェース更新

--- madoka_behold_the_another_world/Assets/Scripts/Battle/BattleInterfaceController.cs
+++ madoka_behold_the_another_world/Assets/Scripts/Battle/BattleInterfaceController.cs
@@ -1,15 +1,190 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
 
-public class BattleInterfaceController : MonoBehaviour {
+public class BattleInterfaceController : MonoBehaviour
+{
+    /// <summary>
+    /// キャラクター名表示部分
+    /// </summary>
+    public Text CharacterName;
+
+    /// <summary>
+    /// キャラクターグラフィック表示部分
+    /// </summary>
+    public PlayerBG Playerbg;
+
+    /// <summary>
+    /// プレイヤーのHP表示部分
+    /// </summary>
+    public Text PlayerHP;
+
+    /// <summary>
+    /// 現在のプレイヤーHP
+    /// </summary>
+    public int NowPlayerHP;
+
+    /// <summary>
+    /// 現在のプレイヤー最大HP
+    /// </summary>
+    public int MaxPlayerHP;
+
+    /// <summary>
+    /// プレイヤーのレベル表示部分
+    /// </summary>
+    public Text PlayerLv;
+
+    public int NowPlayerLV;
+
+    /// <summary>
+    /// 僚機１キャラクターグラフィック
+    /// </summary>
+    public PlayerBG Ally1BG;
+
+    /// <summary>
+    /// 僚機１HP
+    /// </summary>
+    public Text Ally1HP;
+    
+    /// <summary>
+    /// 僚機２キャラクターグラフィック
+    /// </summary>
+    public PlayerBG Ally2BG;
+
+    /// <summary>
+    /// 僚機２HP
+    /// </summary>
+    public Text Ally2HP;
+
+    /// <summary>
+    /// Information表示部分
+    /// </summary>
+    public Text InformatinText;
+
+    /// <summary>
+    /// 覚醒ゲージ
+    /// </summary>
+    public Image ArousalGauge;
+
+    /// <summary>
+    /// 現在の覚醒ゲージ
+    /// </summary>
+    public float NowArousal;
+
+    /// <summary>
+    /// 覚醒ゲージ最大量
+    /// </summary>
+    public float MaxArousal;
+
+    /// <summary>
+    /// 覚醒可能最小値
+    /// </summary>
+    public float BiasArousal;
+
+    /// <summary>
+    /// 覚醒使用可否表示
+    /// </summary>
+    public Text OK;
+
+    /// <summary>
+    /// ブーストゲージ
+    /// </summary>
+    public Image BoostGauge;
+
+    /// <summary>
+    /// 現在のブースト量
+    /// </summary>
+    public float NowBoost;
+
+    /// <summary>
+    /// 最大のブースト量
+    /// </summary>
+    public float MaxBoost;
+
+    /// <summary>
+    /// ウェポンゲージ最下段
+    /// </summary>
+    public WeaponGauge Weapon1;
+
+    /// <summary>
+    /// 以下下から順
+    /// </summary>
+    public WeaponGauge Weapon2;
+
+    public WeaponGauge Weapon3;
+
+    public WeaponGauge Weapon4;
+
+    public WeaponGauge Weapon5;
+
+    /// <summary>
+    /// アイテム名表示部
+    /// </summary>
+    public Text ItemName;
+
+    /// <summary>
+    /// 装備アイテム名
+    /// </summary>
+    public string Itemname;
+
+    /// <summary>
+    /// アイテム個数
+    /// </summary>
+    public Text ItemNumber;
+
+    public int Itemnumber;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        this.UpdateAsObservable().Subscribe(_ => 
+        {
+            // 覚醒ゲージ表示
+            ArousalGauge.fillAmount = NowArousal / MaxArousal;
+            // 閾値を超えていたらOKを表示
+            if(NowArousal > BiasArousal)
+            {
+                OK.gameObject.SetActive(true);
+            }
+            else
+            {
+                OK.gameObject.SetActive(false);
+            }
+            // ブーストゲージを表示
+            BoostGauge.fillAmount = NowBoost / MaxBoost;
+
+            // HP表示
+            PlayerHP.text = NowPlayerHP.ToString();
+
+            // HPが1/4を割っていたら赤色
+            if(NowPlayerHP < 0.25f * MaxPlayerHP)
+            {
+                PlayerHP.color = new Color(1.0f, 0.0f, 0.0f);
+            }
+            // HPが1/2を割っていたら黄色
+            else if (NowPlayerHP < 0.5f * MaxPlayerHP)
+            {
+                PlayerHP.color = new Color(0.96f, 1.0f, 0.21f);
+            }
+            // それ以外なら黒
+            else
+            {
+                PlayerHP.color = new Color(1.0f, 1.0f, 1.0f);
+            }
+
+            // レベル表示
+            PlayerLv.text = "LEVEL - " + NowPlayerLV.ToString();
+
+            // 
+        });
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
--- madoka_behold_the_another_world/Assets/Scripts/Battle/WeaponGauge.cs
+++ madoka_behold_the_another_world/Assets/Scripts/Battle/WeaponGauge.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UniRx;
+using UniRx.Triggers;
 
 public class WeaponGauge : MonoBehaviour 
 {
@@ -19,6 +21,11 @@ public class WeaponGauge : MonoBehaviour
 	/// </summary>
 	public bool UseChargeGauge;
 
+    /// <summary>
+    /// 現在のチャージ量（最大１）
+    /// </summary>
+    public float NowChargeValue;
+
 	/// <summary>
 	/// リロードゲージ
 	/// </summary>
@@ -35,14 +42,54 @@ public class WeaponGauge : MonoBehaviour
 	public float MaxBulletNumber;
 
 	/// <summary>
-	/// 現在の弾数
+	/// 現在の弾数(リロードは小数で行い、フレームごとにいくつ回復するか決める）
 	/// </summary>
 	public float NowBulletNumber;
+
+    /// <summary>
+    /// 使用可能か否か（撃ちきりフルリロードなどの場合、このフラグを折っておく）
+    /// </summary>
+    public bool Use;
+
+    /// <summary>
+    /// 残弾数の表示桁数
+    /// </summary>
+    public int Digits;
+
+    public Text Kind;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+        this.UpdateAsObservable().Subscribe(_ => 
+        {
+            // 使用可能の場合、文字列を白に
+            if(Use)
+            {
+                BulletNumber.color = new Color(1, 1, 1);
+            }
+            // 使用不能の場合、文字列を赤に
+            else
+            {
+                BulletNumber.color = new Color(1, 0, 0);
+            }
+            // 現在の弾数を表示
+            BulletNumber.text = NowBulletNumber.ToString("d" + Digits.ToString());
+            // リロード分を表示
+            Reload.fillAmount = NowBulletNumber / MaxBulletNumber;
+            // チャージを使う場合
+            if (UseChargeGauge)
+            {
+                Charge.gameObject.SetActive(true);
+                // 現在のチャージ量を表示
+                Charge.fillAmount = NowChargeValue;
+            }
+            else
+            {
+                Charge.gameObject.SetActive(false);
+            }
+        });
+	    
 	}
 	
 	// Update is called once per frame
