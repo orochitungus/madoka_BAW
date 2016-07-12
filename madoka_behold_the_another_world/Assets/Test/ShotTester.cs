33cb76d82a525ee844bb57c743e676884bcd359f
OROCHI <tungus_v@yahoo.co.jp>
2016-07-13 01:15:05 +0900

    テストシーンを追加

---
+++ madoka_behold_the_another_world/Assets/Test/ShotTester.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotTester : MonoBehaviour 
+{
+
+	public Animator AnimatorUnit;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			AnimatorUnit.SetInteger("NowState", 1);
+		}
+	}
+
+	void LateUpdate()
+	{
+		AnimatorUnit.SetInteger("NowState", -1);
+	}
+
+	public void FollowThrowDone()
+	{
+		AnimatorUnit.SetInteger("NowStete", 2);
+	}
+
+	public void ReturnToIdle()
+	{
+		AnimatorUnit.SetInteger("NowState", 0);
+	}
+}
