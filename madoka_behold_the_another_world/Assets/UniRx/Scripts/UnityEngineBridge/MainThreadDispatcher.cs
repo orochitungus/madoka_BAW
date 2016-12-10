e9b42c1dd4daaf0fb0ce5d66f7506b03af326272
OROCHI <tungus_v@yahoo.co.jp>
2016-12-11 02:05:23 +0900

    CPUゲージ関連追加

--- madoka_behold_the_another_world/Assets/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
+++ madoka_behold_the_another_world/Assets/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
@@ -255,7 +255,7 @@ namespace UniRx
                         var distpacher2 = Instance;
                         if (distpacher2 != null)
                         {
-                            distpacher2.StartCoroutine_Auto(routine);
+                            distpacher2.StartCoroutine(routine);
                         }
                     });
                 }
@@ -271,7 +271,7 @@ namespace UniRx
             var dispatcher = Instance;
             if (dispatcher != null)
             {
-                return dispatcher.StartCoroutine_Auto(routine);
+                return dispatcher.StartCoroutine(routine);
             }
             else
             {
