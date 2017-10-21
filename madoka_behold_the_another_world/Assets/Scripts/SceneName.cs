f1e570ede2343c54a40307449d6815e14772b86a
OROCHI <tungus_v@yahoo.co.jp>
2017-10-21 16:01:31 +0900

    メニュー画面を実装

--- madoka_behold_the_another_world/Assets/Scripts/SceneName.cs
+++ madoka_behold_the_another_world/Assets/Scripts/SceneName.cs
@@ -7,16 +7,14 @@ public static class SceneName
     public static string[] sceneName = new string[]
     {
         "",                                         // ダミー
-        "Broken_Mitakihara",                        // 1:崩壊見滝原1
-        "Broken_Mitakihara2",                       // 2:崩壊見滝原2
-        "Imagica_Shock_Ground_Zero",                // 3:イマジカショック・グラウンドゼロ
-        "Imagica_Shock_Ground_Zero_VSScono",        // 4:イマジカショック・グラウンドゼロ（VSスコノシュート）
-        "Mitakihara_Hospital_HomuraRoom",           // 5:見滝原病院・ほむらの部屋
-        "Mitakihara_Hospital_56F",                  // 6:見滝原病院・56階
-		"Mitakihara_Hospital_106F",					// 7:見滝原病院・106階
-		"Mitakihara_Hospital_150F",					// 8:見滝原病院・150階（屋上)
-		"Mitakihara_Hospital_Entrance",				// 9:見滝原病院・1階
-		"Mitakihara_Hospital_Bycicleshed",			// 10:見滝原病院・入口
-		"",											// 11:見滝原病院・恭介の部屋
+        "BrokenMitakihara",							// 1:崩壊見滝原1
+        "Imagica_Shock_Ground_Zero_VSScono",		// 2:イマジカショック・グラウンドゼロ（VSスコノシュート）
+        "Mitakihara_Hospital_HomuraRoom",			// 3:見滝原病院・ほむらの部屋
+        "Mitakihara_Hospital_56F",					// 4:見滝原病院・56階
+		"Mitakihara_Hospital_106F",					// 5:見滝原病院・106階
+		"Mitakihara_Hospital_150F",					// 6:見滝原病院・150階（屋上)
+		"Mitakihara_Hospital_Entrance",				// 7:見滝原病院・1階
+		"Mitakihara_Hospital_Bycicleshed",			// 8:見滝原病院・入口
+		"",											// 9:見滝原病院・恭介の部屋
     };
 }
